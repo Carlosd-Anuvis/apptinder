33429d9afe0303531a6cd859cf7c85a9812d4afd
deros <carloscariilo032@gmail.com>
2023-08-24 15:55:06 -0500

    se envia proyecto

---
+++ index.js
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import { sequelize } from './database/DBConnection.js';
+// import { User } from './models/User.js';
+// import { Skill } from './models/Skill.js';
+
+import app from './app.js';
+const port = 3000 || process.env.PORT;
+
+
+(async () => {
+  try {
+    await sequelize.sync({ force: false});
+    app.listen(port, () => console.log(`Server is running on port ${port}`))
+  } catch (error) {
+    console.error(error);
+  }
+})()
+
+
+
+
+
+
+
---
+++ src/app.js
@@ -0,0 +1,14 @@
+
+
+import express from "express"
+const app  = express();
+
+app. use(express.json());
+
+app.get('/', (req, res) => {
+    res.send ('hola carlos')
+
+
+})
+
+export default app;
